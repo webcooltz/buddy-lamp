6538bc81e493352f52b3f0de59b5a74d63d9090a
Tyler Tucker <webcooltz@duck.com>
2023-06-26 10:38:23 -0600

    Code commenting/cleanup
    -Added good comments, cleaned up the code.
    -Changed button_pressed to false in the callback function after the change_color() func is called.

--- client/button-test.py
+++ client/button-test.py
@@ -6,12 +6,14 @@ GPIO.setmode(GPIO.BCM)
 # Define the GPIO pin connected to the button
 button_pin = 2
 
-# Setup the button pin as an input
+# Set up the button pin as an input
 GPIO.setup(button_pin, GPIO.IN)
-   
+
+
 def button_callback(channel):
     print('Button pressed')
     print('Pin: ' + str(button_pin))
+
 
 # Add event detection for button press
 GPIO.add_event_detect(button_pin, GPIO.FALLING, callback=button_callback, bouncetime=300)
--- client/main.py
+++ client/main.py
@@ -1,59 +1,86 @@
+# Main - Buddy Lamp
+# get_color():
 # -gets server color every 10 minutes
+# change_color():
 # -sets server color to your color when button is pressed
+# button_callback():
+# -when button is pressed, calls change_color()
 
 import time
 import requests
+
 try:
     import RPi.GPIO as GPIO
 except ModuleNotFoundError:
     print("RPi module not found. Are you running on a Raspberry Pi?")
 
-# change these
+# *---change these---*
 api_url = "https://URL"
 my_household_name = "Johnson"
 my_color = "red"
+# ****
+
 current_color = ""
 
-# GPIO stuff
+# ---GPIO stuff---
 GPIO.setmode(GPIO.BCM)
-# button
+# Button
 button_pin = 2  # GPIO pin connected to the button
-GPIO.setup(button_pin, GPIO.IN)
+GPIO.setup(button_pin, GPIO.IN)  # Set the button pin as an input
 button_pressed = False  # Flag to track button state
-# LED
-led_pin_number = 3 # red
+# LEDs
+led_pin_number = 3  # red
 GPIO.setup(led_pin_number, GPIO.OUT)  # Set the LED pin as an output
-led_pin_number2 = 17 # blue
+led_pin_number2 = 17  # blue
 GPIO.setup(led_pin_number2, GPIO.OUT)
 
 
 def get_color(color_currently, led_pin, led_pin2):
+    """
+        Gets the color from the server and adjusts the LEDs.
+
+        Args:
+            color_currently (str): The color the server currently has.
+            led_pin (int): The first LED GPIO number.
+            led_pin2 (int): The second LED GPIO number.
+
+        Returns:
+            color_currently (str): The new server color.
+    """
     try:
+        # HTTP GET request to the server
         response = requests.get(api_url)
+        # if the response succeeded (200 code)
         if response.status_code == 200:
-            print("200: Request successful!") 
+            print("200: Request successful!")
             data = response.json()
             server_color = data.get("color")
             print("server_color: " + server_color)
+            # if the color is white: turn off RGBs
             if server_color == "white":
                 print(f"no server color")
                 GPIO.output(led_pin, GPIO.LOW)  # Turn off the 1st LED (yours)
                 GPIO.output(led_pin2, GPIO.LOW)  # Turn off the 2nd LED (other)
+            # if the color is not your color: turn off your color, turn on the right color
             elif server_color and server_color != my_color:
                 color_currently = server_color
                 print(f"color updated to {server_color}")
-                GPIO.output(led_pin, GPIO.LOW)  # Turn off the 1st LED (yours)
-                GPIO.output(led_pin2, GPIO.HIGH)  # Turn on the 2nd LED (other)
+                GPIO.output(led_pin, GPIO.LOW)
+                GPIO.output(led_pin2, GPIO.HIGH)
+            # if the color is not your color and has not changed: do nothing
             elif server_color and server_color != my_color and server_color == color_currently:
                 print(f"server color has not changed")
+            # if the color is your color: turn off other LED, turn on your LED
             elif server_color and server_color == my_color:
                 print(f"server color is your color")
-                GPIO.output(led_pin2, GPIO.LOW)  # Turn off the 2nd LED (other)
-                GPIO.output(led_pin, GPIO.HIGH)  # Turn on the 1st LED (yours)
+                GPIO.output(led_pin2, GPIO.LOW)
+                GPIO.output(led_pin, GPIO.HIGH)
+            # other cases that are not handled
             else:
-                print(f"Other case")
+                print(f"Other case - not yet handled")
+        # if code != 200
         else:
-            print("Request failed!")  # Handle the request failure scenario
+            print("Request failed!")
     except requests.exceptions.RequestException as e:
         print(f"Request error: {e}")
 
@@ -61,38 +88,65 @@ def get_color(color_currently, led_pin, led_pin2):
 
 
 def change_color(household_name, color, led_pin, led_pin2):
+    """
+        Sends your color to the server and adjusts the LEDs.
+
+        Args:
+            household_name (str): Your household name.
+            color (str): The color you are sending.
+            led_pin (int): The first LED GPIO number.
+            led_pin2 (int): The second LED GPIO number.
+
+        Returns:
+            Nothing.
+    """
     try:
         payload = {
-          "household": {
-            "name": household_name,
-            "color": color
-          }
+            "household": {
+                "name": household_name,
+                "color": color
+            }
         }
-        response = requests.post(api_url, json=payload)  # Send the HTTP POST request
-        if response.status_code == 201:  # Check if the request was successful
-            GPIO.output(led_pin2, GPIO.LOW)  # Turn off the 2nd LED (other)
-            GPIO.output(led_pin, GPIO.HIGH)  # Turn on 1st the LED (yours)
+        # HTTP POST request to the server
+        response = requests.post(api_url, json=payload)
+        # if the request succeeded: Turn other LEDs off, turn your LED on
+        if response.status_code == 201:
+            GPIO.output(led_pin2, GPIO.LOW)
+            GPIO.output(led_pin, GPIO.HIGH)
             print("201: Color change request successful!")
         else:
-            print("Color change request failed!")  # Handle the request failure scenario
+            print("Color change request failed!")
     except requests.exceptions.RequestException as e:
         print(f"Request error: {e}")
 
 
-def button_callback(channel, household_name, color, led_pin, led_pin2):
-    # allows button to only be triggered on release
+def button_callback(household_name, color, led_pin, led_pin2):
+    """
+        When button is pressed: Calls change_color().
+
+        Args:
+            household_name (str): Your household name.
+            color (str): The color you are sending.
+            led_pin (int): The first LED GPIO number.
+            led_pin2 (int): The second LED GPIO number.
+
+        Returns:
+            Nothing.
+    """
+    # Allows button to be triggered only on release
     global button_pressed
     if not button_pressed:
         button_pressed = True
         change_color(household_name, color, led_pin, led_pin2)
+    button_pressed = False  # Reset button state
 
 
-GPIO.add_event_detect(button_pin, GPIO.FALLING, callback=lambda channel: button_callback(channel, my_household_name, my_color, led_pin_number, led_pin_number2), bouncetime=300)
+# Button listener
+GPIO.add_event_detect(button_pin, GPIO.FALLING,
+                      callback=lambda channel: button_callback(my_household_name, my_color, led_pin_number,
+                                                               led_pin_number2), bouncetime=300)
 
+# Loop to keep it listening/requesting
 while True:
     current_color = get_color(current_color, led_pin_number, led_pin_number2)
     time.sleep(10)  # Wait for 10 minutes (600 seconds) before the next request
-    button_pressed = False  # Reset button state
-
-# Cleanup GPIO
-GPIO.cleanup()
